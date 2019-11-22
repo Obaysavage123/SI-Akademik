afe156346270c00ff5ca8dd133b37d655dd48737
THORIQSULTHON <55569625+THORIQSULTHON@users.noreply.github.com>
2019-11-22 20:03:22 +0700

    Add files via upload

--- head.php
+++ head.php
@@ -6,7 +6,7 @@ include_once "koneksi.php";
 ?>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>SIAPORT | SD Jemberlor 1</title>
+  <title>SIAPOD | SD Jemberlor 1</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- Font Awesome -->
@@ -30,3 +30,122 @@ include_once "koneksi.php";
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
+<?php
+session_start();
+$sql = mysqli_query($koneksi,"select * From tb_guru where username='".$_SESSION['username']."'");
+$data = mysqli_fetch_assoc($sql);
+?> 
+
+<body class="hold-transition sidebar-mini layout-fixed">
+<div class="wrapper">
+
+  <!-- Navbar -->
+  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+<!-- Left navbar links -->
+<ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
+</li>
+    </ul>
+    
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+      <!-- Messages Dropdown Menu -->
+      <li class="nav-item dropdown">
+        <a class="nav-link" data-toggle="dropdown" href="#">
+          <i class="far fa-comments"></i>
+          
+        </a>
+        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+          <a href="logout.php" class="dropdown-item">
+            <!-- Message Start -->
+            <div class="media">
+              <div class="media-body">
+                <h3 class="dropdown-item-title">
+                 Logout
+                </h3>
+              </div>
+            </div>
+     
+      <li class="nav-item">
+        <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#">
+          <i class="fas fa-th-large"></i>
+        </a>
+      </li>
+    </ul>
+  </nav>
+  <!-- /.navbar -->
+ <!-- Main Sidebar Container -->
+ <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="index.html" class="brand-link">
+      <img src="" alt=" Logo" class="brand-image img-circle elevation-3"
+           style="opacity: .8">
+      <span class="brand-text font-weight-light">SIAPORT</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div  class="info">
+        <a href="#" class="d-block"><?php echo $data['nama'];?></a>
+        </div>
+      </div>
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+
+          <li class="nav-item">
+                <a href="./halaman_admin.php" class="nav-link active">
+                  <i class="nav-icon fas fa-tachometer-alt"></i>
+                  <p>Dashboard </p>
+                </a>
+              </li>
+              <li class="nav-header">Hak Akses Anda</li>
+              <li class="nav-item">
+                <a href="pages/Tambah_User.php" class="nav-link">
+                  <i class="nav-icon fas fa-plus-square"></i>
+                  <p>
+                   Tambah User
+                  </p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="pages/.php" class="nav-link">
+                  <i class="nav-icon far fa-address-card"></i>
+                  <p>
+                   Data Guru
+                  </p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="pages/calendar.php" class="nav-link">
+                  <i class="nav-icon fas fa-address-card"></i>
+                  <p>
+                   Data Siswa
+                  </p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="pages/calendar.php" class="nav-link">
+                  <i class="nav-icon fas fa-print"></i>
+                  <p>
+                    Cetak Raport
+                  </p>
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <!-- /.sidebar-menu -->
+        </div>
+        <!-- /.sidebar -->
+      </aside>
+      </body?>
+      </html>
